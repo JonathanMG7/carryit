6c85c82ad3c6842e8fba1d7136a40e6d8400f7fa
Jonathan <jonathan@Jonathan-Toshiba.(none)>
2015-10-11 21:07:37 -0500

    Agregada seccion inicio

--- solicitudes/urls.py
+++ solicitudes/urls.py
@@ -4,10 +4,5 @@ from solicitudes import views
 urlpatterns = (
     url(r'^$', views.index, name='index'),
     url(r'^lista/', views.lista, name='lista'),
-<<<<<<< HEAD
-    url(r'^listas/',views.lista_asJson, name='listas'),
-    )
-=======
     url(r'^listas/', views.lista_asJson, name='listas'),
 )
->>>>>>> a572ff2ce480deebf211bd9e6e24bf01a14ed113
